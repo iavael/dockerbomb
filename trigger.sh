6f6d1bbabdcbd10c6f2f1bdcf375d1f5c9acc4cb
Iavael <905853+iavael@users.noreply.github.com>
2023-01-17 12:52:45 +0400

    Update trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --userns=host --rm -v /var/run/docker.sock:/var/run/docker.sock docker.io/iavael/dockerbomb
+docker run --userns=host --rm -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/iavael/dockerbomb:latest
